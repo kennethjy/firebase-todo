fa40946391ca5645958c61100333e18590614a44
kenneth <kennethjayadiyu@gmail.com>
2024-03-18 19:33:20 +0700

    t

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ function App() {
       <div class="todo-container">
         {renderedOutput}
         <div class="addButton" onClick={appendlist}>
-          <img src='src/plus.png'></img>
+          <img src='/plus.png'></img>
         </div>
       </div>
       
