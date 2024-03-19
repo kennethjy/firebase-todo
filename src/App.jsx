4419a14d371d19c12a144049e7a1f7a524ab162a
kenneth <kennethjayadiyu@gmail.com>
2024-03-19 20:20:49 +0700

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
+import { CgAddR } from "react-icons/cg";
+import { FiX, FiFilter } from "react-icons/fi";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,10 +9,19 @@ function App() {
     const storedArr = localStorage.getItem('myArr');
     return storedArr ? JSON.parse(storedArr) : [];
   });
+  const [filterOption, setFilterOption] = useState('all');
   
   useEffect(() => {
     localStorage.setItem('myArr', JSON.stringify(arr));
   }, [arr]);
+
+  useEffect(() => {
+    const storedArr = JSON.parse(localStorage.getItem('myArr'));
+    if (storedArr) {
+      const sortedArr = storedArr.sort((a, b) => (a.isChecked === b.isChecked ? 0 : a.isChecked ? 1 : -1));
+      setArr(storedArr);
+    }
+  }, []);
 
   function appendlist() {
     const newItem = {
@@ -35,11 +46,38 @@ function App() {
     newArr[index].isChecked = !newArr[index].isChecked;
     setArr(newArr);
   }
+  function filterArray() {
+    switch (filterOption) {
+      case 'checked':
+        return arr.filter(item => item.isChecked);
+      case 'unchecked':
+        return arr.filter(item => !item.isChecked);
+      default:
+        return arr;
+    }
+  }
 
+  useEffect(
+    () => {arr.sort((a, b) => (a.isChecked === b.isChecked ? 0 : a.isChecked ? 1 : -1))},
+    [filterOption]
+  )
 
-  const renderedOutput = arr
-  .sort((a, b) => (a.isChecked === b.isChecked ? 0 : a.isChecked ? 1 : -1))
-  .map((item, index) => (
+  // JSX for the filter module
+  const filterModule = (
+    <div class="filter">
+      <FiFilter size={30}/>
+      <select class="select-dropdown" value={filterOption} onChange={(e) => setFilterOption(e.target.value)}>
+        <option value="all">Show All</option>
+        <option value="checked">Show Checked</option>
+        <option value="unchecked">Show Unchecked</option>
+      </select>
+    </div>
+  );
+
+  // Filter the array based on the filter option
+  const filteredArr = filterArray();
+
+  const renderedOutput = filteredArr.map((item, index) => (
       <div key={index} class="todo-item">
         <div class="left-todo">
         <div class="checkbox-outer" onClick={() => changeCheck(index)}>
@@ -54,7 +92,7 @@ function App() {
         </div>
         </div>
         <button onClick={() => removeTodo(index)} class="removeButton">
-          X
+          <FiX size={30}/>
         </button>
     </div>
   ));
@@ -63,12 +101,13 @@ function App() {
 
   return (
     <>
+      <div class="todo-container">
       <h1>To-do List</h1>
       <p>by: Kenneth Jayadi Yu 2602158260</p>
-      <div class="todo-container">
+        {filterModule}
         {renderedOutput}
         <div class="addButton" onClick={appendlist}>
-          +
+          <CgAddR size={30}/>
         </div>
       </div>
       
